ca784e9e176609c30fb89351e3e05693bd954236
pinakigitub <pinaki.best@gmail.com>
2016-06-05 03:23:36 -0700

    Add files via upload

---
+++ AuthForWebServices/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using AuthForWebServices.MyservicyProxy;
+
+namespace AuthForWebServices
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            AuthForWebServices.MyservicyProxy.WebService webService = new MyservicyProxy.WebService();
+            AuthForWebServices.MyservicyProxy.AuthHeader authentication = new MyservicyProxy.AuthHeader();
+            authentication.Username = "test111";
+            authentication.Password = "test111";
+            webService.AuthHeaderValue = authentication;
+            string data = webService.SensitiveData();
+            var fgg = data;
+        }
+    }
+}
---
+++ AuthForWebServices/WebService.asmx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace AuthForWebServices
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+     [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+        public AuthHeader Authentication;
+
+        public WebService()
+        {
+            
+            InitializeComponent();
+        }
+
+        private IContainer components = null;
+
+        private void InitializeComponent()
+        {
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && components != null)
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [SoapHeader("Authentication", Required = true)]
+        [WebMethod(Description = "Returns some sample data")]
+        public string SensitiveData()
+        {
+            string data = null;
+            if (Authentication.Username == "test" && Authentication.Password == "test")
+            {
+                data = "Success";
+            }
+            else
+            {
+                data = "Error";
+            }
+            return data;
+        }
+    }
+
+    public class AuthHeader : SoapHeader
+    {
+        public string Username {get;set;}
+        public string Password {get;set;}
+    }
+}
